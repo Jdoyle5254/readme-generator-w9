aaff24dc7c8913adeaedfe48d1271997a2e2e953
Jen Doyle <jdoyle5254@users.noreply.github.com>
2020-11-29 13:41:56 -0600

    added additonal license and badge to options

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ inquirer
                name: 'Apache',
             },
             {
-              name: '',
+              name: 'Unilicense',
             },
         ],
 
@@ -97,6 +97,11 @@ inquirer
           else if (response.license == 'Apache'){ 
             licenseDetails = " Copyright [yyyy] [name of copyright owner]\r \t \t Licensed under the Apache License, Version 2.0 (the \"License\"); \r   you may not use this file except in compliance with the License. \r You may obtain a copy of the License at \r \t  http://www.apache.org/licenses/LICENSE-2.0  \r \t Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and  limitations under the License.";
             licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+
+          }
+          else if (response.license == 'Unilicense'){ 
+            licenseDetails = "This is free and unencumbered software released into the public domain.\r Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.\r In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this   software under copyright law. \r THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. For more information, please refer to <https://unlicense.org>";
+            licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/) ";
 
           }
     
